fbdfb209c54d42dd624acfd2ad7308e35f98ceff
Dante451 <dantedaciuk555@gmail.com>
2024-04-03 16:51:55 -0500

    Created init with four attributes

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -5,5 +5,25 @@ Date: {Date}
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
+from mortgage.pixell_lookup import MortgageRate, PaymentFrequency, VALID_AMORTIZATION 
+def __init__(self, loan_amount, rate, frequency, amortization):
 
+    if loan_amount <= 0:
+        raise ValueError("Loan amount must be positive")
+    else:
+        self.__loan_amount = loan_amount
 
+    try:
+        self.__rate = MortgageRate[rate]
+    except Exception as e:
+        raise ValueError("Rate provided is invalid.")
+    
+    try:
+        self.__frequency = PaymentFrequency[frequency]
+    except Exception as e:
+        raise ValueError("Frequency provided is invalid.")
+    
+    if amortization not in VALID_AMORTIZATION:
+        raise ValueError("Amortization provided is invalid")
+    else:
+        self.__amortization = amortization
