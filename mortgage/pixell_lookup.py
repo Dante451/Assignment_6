eb8c937c6969867af43f7e15ff3e5346a441a4ef
Dante451 <dantedaciuk555@gmail.com>
2024-04-03 15:22:59 -0500

    Created two classes and added mortgage variables to them

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -8,9 +8,13 @@ Usage: The enumerations and list in this file may be used when working
 with mortgages to ensure only valid rates, frequencies and amortization 
 periods are used.
 """
-#Commit staging
 
 from enum import Enum
-
+from mortgage.pixell_lookup import MortgageRate, PaymentFrequency, VALID_AMORTIZATION
 VALID_AMORTIZATION = {5, 10, 15, 20, 25, 30}
 
+class MortgageRate(Enum):
+    def __init__(loan_amount(float), rate(str), frequency(str), amortization(int)):
+    
+
+class PaymentFrequency(Enum):
